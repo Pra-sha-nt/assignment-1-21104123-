e508d9c10dd40bcf40522a6e66fdacafff5e417d
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-01-14 22:28:16 +0530

    Delete ass1_q1.py

--- ass1_q1.py
+++
@@ -1,5 +0,0 @@
-num1=int(input('enter your 1st number\n'))
-num2=int(input('enter your 2nd number\n'))
-num3=int(input('enter your 3rd number\n'))
-x=(num1+num2+num3)/ 3
-print( 'your average is' , x )
