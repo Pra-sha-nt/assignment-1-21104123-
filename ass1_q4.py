6c9e43d627cdfda25621b0b143f8420e75ffacd4
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-01-14 22:28:42 +0530

    Delete ass1_q4.py

--- ass1_q4.py
+++
@@ -1,8 +0,0 @@
-num1=int(input('enter 1st student marks\n'))
-num2=int(input('enter 2nd student marks\n'))
-num3=int(input('enter 3rd student marks\n'))
-num4=int(input('enter 4th student marks\n'))
-num5=int(input('enter 5th student marks\n'))
-x=[num1,num2,num3,num4,num5]
-x.sort()
-print(x)
