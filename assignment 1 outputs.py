03a93ec3c8fa8cb21d17fed55aa0b4adc979979d
Pra-sha-nt <96761913+Pra-sha-nt@users.noreply.github.com>
2022-01-14 11:29:09 +0530

    Add files via upload

---
+++ assignment 1 outputs.py
@@ -0,0 +1,51 @@
+#output
+answer1
+enter your 1st number
+2
+enter your 2nd number
+2
+enter your 3rd number
+2
+your average is 2.0
+
+answer3
+enter your sid
+ 2113151
+enter your name
+prashant
+enetr your course name
+electrical
+enter your cgpa
+9
+[2113151, 'prashant', 'electrical', 9.0]
+
+
+answer2
+enter your gross income
+500000
+enter number of dependents
+13256
+income tax = -7855600.0
+
+
+answer4
+enter 1st student marks
+22
+enter 2nd student marks
+211
+enter 3rd student marks
+125
+enter 4th student marks
+515
+enter 5th student marks
+15
+[15, 22, 125, 211, 515]
+
+answer5(a)
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'pink', 'yellow']
+
+
+answer5(b)
+['red', 'green', 'white', 'black', 'pink', 'yellow']
+['red', 'green', 'white', 'purple', 'yellow']
