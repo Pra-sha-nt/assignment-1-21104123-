0e327dc777de76b1821e50f51d9f5504cdcfc322
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-01-26 23:32:50 +0530

    Add files via upload

---
+++ ass2output.py
@@ -0,0 +1,57 @@
+output1
+36
+ eguagnal evitisnes esac a si nohtyp
+a case sensitive
+python is object orentated langauge
+10
+pythonisacasesensitivelangauge
+
+output2
+enter your name
+prashant
+enter your sid
+21104123
+enter your branch
+ee
+enter your CGPA
+9
+Hey, prashant Here!
+My SID is 21104123
+I am from ee and my CGPA is 9
+
+
+output3
+8
+58
+50
+224
+40
+14
+0
+
+
+output4
+enter 1st no.
+20
+enter 2nd no.
+25
+enter 3rd no.
+23
+greaest no. is  25
+
+
+
+output5
+enter any word you want
+word
+no
+
+
+output6
+side 1st
+20
+side 2nd
+23
+side 3rd
+25
+yes
