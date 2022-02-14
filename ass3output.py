d0c6dea3813396981c78a33a07b6f63ba3b2193e
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-02-15 00:22:07 +0530

    Add files via upload

---
+++ ass3output.py
@@ -0,0 +1,88 @@
+
+#ans1
+enter string:rose are red and sky is blue 
+enter word:is
+count of the word is:
+1
+
+
+#ans2
+Input a year: 2020
+Input a month [1-12]: 5
+Input a day [1-31]: 23
+The next date is [yyyy-mm-dd] 2020-5-24.
+
+
+
+#ans3
+enter lower range:1
+enter upper range:4
+[(1, 1), (2, 4), (3, 9), (4, 16)]
+
+
+
+#ans4
+enter grade point:9
+your grade A
+
+
+
+#ans5
+Enter how many lines? 6
+     A
+    ABC
+   ABCDE
+  ABCDEFG
+ ABCDEFGHI
+ABCDEFGHIJK
+
+
+
+#ans6
+enter number of students:2
+enter student name:joymonk 
+enter student SID:131161
+Dictionary is {'joymonk': 131161}
+enter student name:jerry
+enter student SID:56951
+Dictionary is {'joymonk': 131161, 'jerry': 56951}
+
+
+
+
+
+#ans7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+
+
+
+
+#ans8
+part a: {1, 3, 5, 6, 8}
+partb: {3, 6, 8, 9, 13, 17}
+part c: {1, 2, 4, 5}
+part d: {1}
+part d: {1, 2}
+part d: {1, 2, 3}
+part d: {1, 2, 3, 4}
+part d: {1, 2, 3, 4, 5}
+part e: {1}
+part e: {1, 2}
+part e: {1, 2, 3}
+part e: {1, 2, 3, 4}
+part e: {1, 2, 3, 4, 5}
+part e: {1, 2, 3, 4, 5, 6}
+part e: {1, 2, 3, 4, 5, 6, 8}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
