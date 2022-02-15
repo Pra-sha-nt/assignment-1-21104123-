0ab1bcd536f2d03b9b26ca6cd8cbcf033cbfbc18
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-02-15 23:29:50 +0530

    Add files via upload

---
+++ assi3output.py
@@ -0,0 +1,92 @@
+
+
+#ans1
+enter string:super car at great speed than ordinary car
+enter word:car
+count of the word is:
+2
+
+
+#ans2
+Input a year: 2020
+Input a month [1-12]: 6
+Input a day [1-31]: 7
+The next date is [yyyy-mm-dd] 2020-6-8.
+
+
+#ans3
+enter lower range:1
+enter upper range:4
+[(1, 1), (2, 4), (3, 9), (4, 16)]
+
+#ans4
+enter grade point:9
+your grade A
+
+#ans5
+Enter number of rows required: 6
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+
+
+#ans6
+enter number of students:3
+enter student name:jeo
+enter student SID:20
+
+enter student name:leo
+enter student SID:10
+
+enter student name:tobi
+enter student SID:30
+Dictionary is {'jeo': 20, 'leo': 10, 'tobi': 30}
+
+leo 10
+jeo 20
+tobi 30
+
+
+enter sid to search:30
+name: tobi
+
+
+
+#ans7
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+
+
+
+#ans8
+part a: {1, 3, 5, 6, 8}
+partb: {3, 6, 8, 9, 13, 17}
+part c: {1, 2, 4, 5}
+part d: {1}
+part d: {1, 2}
+part d: {1, 2, 3}
+part d: {1, 2, 3, 4}
+part d: {1, 2, 3, 4, 5}
+part e: {1}
+part e: {1, 2}
+part e: {1, 2, 3}
+part e: {1, 2, 3, 4}
+part e: {1, 2, 3, 4, 5}
+part e: {1, 2, 3, 4, 5, 6}
+part e: {1, 2, 3, 4, 5, 6, 8}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
+part e: {1, 2, 3, 4, 5, 6, 8, 9}
