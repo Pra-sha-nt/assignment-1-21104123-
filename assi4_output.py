b8818c5f96ca6640f64f97ce4865e956bf91f231
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-03-02 13:05:52 +0530

    Add files via upload

---
+++ assi4_output.py
@@ -0,0 +1,53 @@
+#ans1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+
+
+#ans2
+enter no of rows:5
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+
+
+
+#ans3
+enter your 1st number:4
+enter your 2nd number:2
+Quotient:  2
+Remainder 0
+
+ans3(b)
+all values are non zero
+
+
+
+#ans4
+my name is  Prashant and my roll no. is  21
+
+
+
+
+#ans5
+Employe name: Mehak  , Employe salary: 70000
+Employe name: Ashok  , Employe salary: 50000
+Employe name: Viren  , Employe salary: 60000
+
+
+
+
+
+#ans6
+Type a word!
+fire
+irfr
