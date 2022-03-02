a07feff644dcaf050c24c3f8bfeac43ca0ec4141
Prashant <96761913+Pra-sha-nt@users.noreply.github.com>
2022-03-02 13:35:34 +0530

    Add files via upload

---
+++ assi4_output.py
@@ -0,0 +1,69 @@
+#ans1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+
+
+#ans2
+enter no of rows:5
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+
+
+
+#ans3
+Enter the first integer: 4
+Enter the first integer: 2
+Remainder is:  0
+Quotient is:  2
+One value is zero
+{6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+Immutable set:  frozenset({8, 5, 6, 7})
+Largest value in the set:  8
+Hash value:  8
+
+
+
+#ans4
+my name is  Prashant and my roll no. is  21
+
+
+
+
+#ans5
+Employe name: Mehak  , Employe salary: 40000
+Employe name: Ashok  , Employe salary: 50000
+Employe name: Viren  , Employe salary: 60000
+
+
+Employe name: Mehak  , Employe salary: 70000
+Employe name: Ashok  , Employe salary: 50000
+Employe name: Viren  , Employe salary: 60000
+
+
+
+
+
+
+
+
+
+#ans6
+Type a word!
+fire
+irfr
